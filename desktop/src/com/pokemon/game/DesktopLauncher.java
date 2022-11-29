75cad6ec0239352d9ef9c9c5fa7b4337dc5f068d
Brayden Klemens <bklemens13@gmail.com>
2022-11-29 14:28:26 -0500

    Pokemon Finished

--- desktop/src/com/pokemon/game/DesktopLauncher.java
+++ desktop/src/com/pokemon/game/DesktopLauncher.java
@@ -1,6 +1,5 @@
 package com.pokemon.game;
 
-import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.pokemon.game.GameFreak;
@@ -9,7 +8,6 @@ import com.pokemon.game.GameFreak;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		//config.setForegroundFPS(60);
 		config.setTitle("Pokemon Leaf Green");
 		config.setWindowedMode(750, 480);
 		new Lwjgl3Application(new GameFreak(), config);
