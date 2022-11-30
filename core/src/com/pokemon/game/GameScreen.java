ac0db2a744fcaa56196c78c626084e058d226093
Brayden Klemens <bklemens13@gmail.com>
2022-11-30 14:50:36 -0500

    Update GameScreen.java

--- core/src/com/pokemon/game/GameScreen.java
+++ core/src/com/pokemon/game/GameScreen.java
@@ -256,6 +256,7 @@ public class GameScreen implements Screen, InputProcessor {
 		 //loads home, re-spawns player with fully healed pokemon
 		if (player.getPokemon().get(0).getHealth() < 1) {
 			fight = false;
+			misty.getPokemon().setHealth(misty.getPokemon().getMaxHealth());
 			loadMap = load.get("Map/house.tmx");
 			location = "home";
 			player.setPosition(100, 610);
